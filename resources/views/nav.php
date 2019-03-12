98f1fd09d5897f741f7d234f2583c09462fcd4b4
Duncan Smith <duncanssmith@gmail.com>
2019-03-12 15:54:07 +0000

    Remove welcome page

--- resources/views/nav.php
+++ resources/views/nav.php
@@ -1,7 +1,6 @@
 <div class="nav">
     <ul class="links">
         <li><a href="/">Artwork</a></li>
-        <li><a href="/welcome">Welcome</a></li>
         <li><a href="/login">Login</a></li>
         <li><a href="/about">About</a></li>
         <li><a href="/artworks">Artworks</a></li>
