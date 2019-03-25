0ccea8313ffc78641049067c71cf26397ae6066e
Duncan Smith <duncanssmith@gmail.com>
2019-03-25 16:53:32 +0000

    Minor cleanup changes

--- app/Http/Controllers/XArtworksController.php
+++ app/Http/Controllers/XArtworksController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Artwork;
 
-class ArtworksController extends Controller
+class XArtworksController extends Controller
 {
     public function index()
     {
